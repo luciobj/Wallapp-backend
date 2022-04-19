0d0c039b3c32c7bf5e129d825651da010bd57432
Lúcio BJ <lbjunq@hotmail.com>
2022-04-19 19:17:30 -0300

    test: fixed method outside function

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -11,7 +11,6 @@ class UserModelTests(APITestCase):
         user = User.objects.create_user(
             username="test", email="test@email.com", password="123456"
         )
-
         assert user.username == "test"
         assert user.email == "test@email.com"
         assert user.is_active
@@ -19,15 +18,14 @@ class UserModelTests(APITestCase):
         assert not user.is_superuser
         assert User.objects.all().count() == user_quantity + 1
 
-
-def test_create_superuser():
-    user_quantity = User.objects.all().count()
-    user = User.objects.create_superuser(
-        username="admin", email="admin@email.com", password="123456"
-    )
-    assert user.username == "admin"
-    assert user.email == "admin@email.com"
-    assert user.is_active
-    assert user.is_staff
-    assert user.is_superuser
-    assert User.objects.all().count() == user_quantity + 1
+    def test_create_superuser(self):
+        user_quantity = User.objects.all().count()
+        user = User.objects.create_superuser(
+            username="admin", email="admin@email.com", password="123456"
+        )
+        assert user.username == "admin"
+        assert user.email == "admin@email.com"
+        assert user.is_active
+        assert user.is_staff
+        assert user.is_superuser
+        assert User.objects.all().count() == user_quantity + 1
