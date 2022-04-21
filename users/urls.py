7c7aa28384a62fff46c421d15f7711a1ac7cf811
Lúcio BJ <lbjunq@hotmail.com>
2022-04-21 17:10:16 -0300

    feat: added registration email sending

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
-from .views import UserViewSet, UserCreateView
+from .views import UserCreateView
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register('users', UserCreateView, basename='users')
-router.register('users', UserViewSet, basename='users')
-
 
 urlpatterns = [
     path('', include(router.urls)),
--- users/views.py
+++ users/views.py
@@ -1,17 +1,48 @@
 from rest_framework import viewsets, mixins
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
-from rest_framework_simplejwt.authentication import JWTAuthentication
+import codecs
+import smtplib
+import os
+import dotenv
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class UserCreateView(viewsets.GenericViewSet, mixins.CreateModelMixin):
     queryset = User.objects.all()
     serializer_class = [UserSerializer]
 
+    class Mailer:
+        def __init__(self, from_email, from_password, to_email):
+            self.from_email = from_email
+            self.from_password = from_password
+            self.to_email = to_email
+            subject = 'Welcome to the Wall App'
+            message = 'Thank you for registering to the Wall App, now you can add a post it to the wall, and alter the ones already on it!'
+            self.body = f"Subject:{subject}\n\n{message}".encode('utf-8')
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = [UserSerializer]    
-    permission_classes  = [IsAuthenticated, IsAdminUser]
-    authentication_classes = [JWTAuthentication]
+        def send_email(self, email):
+            with smtplib.SMTP(
+                "smtp-mail.outlook.com", 587
+            ) as server:
+                server.ehlo()
+                server.starttls()
+                server.login(self.from_email, self.from_password)
+                try:
+                    server.sendmail(self.from_email, email, self.body)
+                except (smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused):
+                    raise ValueError
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            dotenv.load_dotenv()
+            from_email = os.getenv("EMAIL_HOST_USER")
+            from_password = os.getenv("EMAIL_HOST_PASSWORD")
+            to_email = request.data['email']
+            mailer = self.Mailer(from_email, from_password, to_email)
+            mailer.send_email(to_email)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
